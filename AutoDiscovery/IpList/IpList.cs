0dd7ddd76360319bce83408e3c05ea74e8f88069
devgopher <igevdo@mail.ru>
2015-12-31 13:47:26 +0300

    1

--- AutoDiscovery/IpList/IpList.cs
+++ AutoDiscovery/IpList/IpList.cs
@@ -8,11 +8,11 @@ using System.Collections.Generic;
 
 namespace AutoDiscovery
 {
-	public class IpListEventArgs : EventArgs {
-		
+	public class IpListEventArgs : EventArgs {		
 		public IpListEventArgs( string _item ) : base() {
 			Item = _item;
 		}
+		
 		public IpListEventArgs() : this(null) {
 
 		}
@@ -22,11 +22,15 @@ namespace AutoDiscovery
 	
 	/// <summary>
 	/// IpList - is a List class realization
-	/// with an ability to raise events on 
+	/// with an ability to raise events on
 	/// "Add" and "Remove" actions
 	/// </summary>
 	public class IpList : List<String>
 	{
+		public delegate void ListChangedHandler( object sender, IpListEventArgs a );
+		public event ListChangedHandler Added;
+		public event ListChangedHandler Removed;
+		
 		public IpList()
 		{
 		}
@@ -58,10 +62,5 @@ namespace AutoDiscovery
 					Removed( this, new IpListEventArgs(item));
 			}
 		}
-		
-		public delegate void ListChangedHandler( object sender, IpListEventArgs a );
-		
-		public event ListChangedHandler Added;
-		public event ListChangedHandler Removed;
 	}
 }
